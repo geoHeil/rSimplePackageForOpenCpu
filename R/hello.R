f1cb8880782733e5847f449703ce3667ba138e99
geoHeil <e1225063@student.tuwien.ac.at>
2015-06-09 00:22:14 +0200

    simple example

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,12 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+# called devtools::document()  once to build namespace
 
+#' @export
 hello <- function() {
   print("Hello, world!")
+}
+
+#' @export
+getLinearInterpolatedEstimateQuantil <- function(x, type, probs){
+  result = quantile(x=x, type=type, probs=probs)
+  return (result)
 }
